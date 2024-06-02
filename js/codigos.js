02f5f900f1e260465e6d434062220f11418a5c92
arturom27 <157868023+arturom27@users.noreply.github.com>
2024-06-02 16:59:17 -0700

    Add files via upload

---
+++ js/codigos.js
@@ -0,0 +1,11 @@
+// Definir un objeto para almacenar los códigos de cada página
+const codigosPorPagina = {
+    "paginaestudiantes75": {
+        "ESTUDIANTE75": true,
+    },
+    "paginatrabajadores75": {
+        "CAFETERIACETIS75": true,
+    }
+};
+
+console.log("codigosPorPagina cargado:", codigosPorPagina);
