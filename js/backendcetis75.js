1cfa46e41c5e3e08a9bd9cc725df208ff58e302a
arturom27 <157868023+arturom27@users.noreply.github.com>
2025-03-15 14:21:33 -0700

    Delete js directory

--- js/backendcetis75.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const menuItemSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    price: Number
-});
-
-module.exports = mongoose.model('MenuItem', menuItemSchema);
